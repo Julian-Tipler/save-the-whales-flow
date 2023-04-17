b6b93f978e1fa5fe9b867a4fceddc2938e07caa6
Julian <tipler.julian@gmail.com>
2023-04-16 21:26:38 -0500

    small

---
+++ README.txt
@@ -0,0 +1,15 @@
+Features custom node should have:
+* male vs female vs unknown
+* alive vs dead
+* 
+
+Features custom edge should have:
+* connect to other edges
+
+
+Frontend framework:
+* Backend data should basically be the node edge structure. Upon changes, this node structure should also change.
+* Whales will store NO relationship data? I think that's fine for now. That can be added manually by Chloe maybe
+* The only information the node will know about the whale is it will have an ID associated with it.
+* Whale will know nothing about node
+
--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,51 @@ import ReactFlow, {
   useEdgesState,
   addEdge,
   NodeToolbar,
+  BackgroundVariant,
+  Position,
 } from "reactflow";
 
 import "reactflow/dist/style.css";
 
 const initialNodes = [
-  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
-  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
+  {
+    id: "shamu",
+    position: { x: 0, y: 0 },
+    data: { label: "Shamu" },
+    sourcePosition: Position.Right,
+    sourceHandles: [{ id: "shamu-handle-1", position: "right" }],
+  },
+  {
+    id: "shamusWife",
+    position: { x: 300, y: 0 },
+    data: { label: "Shamu's Wife" },
+    targetPosition: Position.Left,
+    targetHandles: [{ id: "shamusWife-handle-4", position: "left" }],
+  },
+  {
+    id: "shamusDaughter",
+    position: { x: 0, y: 100 },
+    data: { label: "Shamu's daughter" },
+  },
 ];
-const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
+const initialEdges = [
+  {
+    id: "e1",
+    source: "shamu",
+    target: "shamusWife",
+    type: "step",
+    sourceHandle: "shamu-handle-1",
+    targetHandle: "shamusWife-handle-4",
+  },
+  { id: "e2", source: "e1", target: "shamusDaughter", type: "step" },
+];
 
 export default function App() {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
   const onConnect = useCallback(
-    (params:any) => setEdges((eds) => addEdge(params, eds)),
+    (params: any) => setEdges((eds) => addEdge(params, eds)),
     [setEdges]
   );
 
@@ -32,9 +61,12 @@ export default function App() {
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
+        fitView
+        zoomOnScroll={false}
+        onNodeClick={(event, node) => console.log(event, node)}
       >
         <NodeToolbar />
-        <Background variant="dots" gap={12} size={1} />
+        <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
       </ReactFlow>
     </div>
   );
