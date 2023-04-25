213cfaa1b5d962cf2ff309cb82d933e2683c976e
Julian <tipler.julian@gmail.com>
2023-04-24 20:45:33 -0500

    card layout

---
+++ src/frontend/WhaleDetails/WhaleDetails.tsx
@@ -0,0 +1,19 @@
+import { Card, CardBody, CardFooter, CardHeader } from "@chakra-ui/react";
+
+import React from 'react'
+
+export const WhaleDetails = () => {
+  return (
+    <Card>
+        <CardHeader>
+
+        </CardHeader>
+        <CardBody>
+
+        </CardBody>
+        <CardFooter>
+            
+        </CardFooter>
+    </Card>
+  )
+}
