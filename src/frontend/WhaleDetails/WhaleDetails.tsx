bebb50a041ea4c57b257dc5319a233f02580c9cc
Julian <tipler.julian@gmail.com>
2023-04-26 23:45:39 -0500

    layout which I have to make at a broader level

--- src/frontend/WhaleDetails/WhaleDetails.tsx
+++ src/frontend/WhaleDetails/WhaleDetails.tsx
@@ -11,16 +11,31 @@ import React from "react";
 
 export const WhaleDetails = () => {
   return (
-    <Grid
-      h="200px"
-      templateRows="repeat(2, 1fr)"
-      templateColumns="repeat(5, 1fr)"
-      gap={4}
-    >
-      <GridItem rowSpan={2} colSpan={1} bg="tomato" />
-      <GridItem colSpan={2} bg="papayawhip" />
-      <GridItem colSpan={2} bg="papayawhip" />
-      <GridItem colSpan={4} bg="tomato" />
-    </Grid>
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <Grid
+        templateAreas={`"header header"
+                  "nav main"
+                  "nav footer"`}
+        gridTemplateRows={"50px 1fr 30px"}
+        gridTemplateColumns={"150px 1fr"}
+        h="200px"
+        gap="1"
+        color="blackAlpha.700"
+        fontWeight="bold"
+      >
+        <GridItem pl="2" bg="orange.300" area={"header"}>
+          Header
+        </GridItem>
+        <GridItem pl="2" bg="pink.300" area={"nav"}>
+          Nav
+        </GridItem>
+        <GridItem pl="2" bg="green.300" area={"main"}>
+          Main
+        </GridItem>
+        <GridItem pl="2" bg="blue.300" area={"footer"}>
+          Footer
+        </GridItem>
+      </Grid>
+    </div>
   );
 };
