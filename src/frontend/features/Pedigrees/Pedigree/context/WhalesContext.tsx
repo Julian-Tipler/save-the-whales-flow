4cdd4f0390445073d3955d49565ae768fe221b57
Julian <tipler.julian@gmail.com>
2023-06-10 17:23:27 -0500

    whales context

---
+++ src/frontend/features/Pedigrees/Pedigree/context/WhalesContext.tsx
@@ -0,0 +1,22 @@
+import React, { useContext } from "react";
+import { Whale } from "../../../../../db/Types/Entities";
+
+type WhalesContextValue = {
+  whales: Whale[];
+  setWhales: React.Dispatch<React.SetStateAction<Whale[]>>;
+};
+
+export const WhalesContext = React.createContext<WhalesContextValue>(
+  {} as WhalesContextValue
+);
+
+export function WhalesProvider({ children }: any) {
+  const [whales, setWhales] = React.useState<Whale[]>([]);
+
+  const value = { whales, setWhales };
+  return (
+    <WhalesContext.Provider value={value}>{children}</WhalesContext.Provider>
+  );
+}
+
+export const useWhalesContext = () => useContext(WhalesContext);
