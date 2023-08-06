a8c3b6346aa6310b78494973d4c520ddcb61fcc0
Julian <tipler.julian@gmail.com>
2023-08-06 12:55:59 -0500

    some pedigree loading logic if pedigree not found

--- src/frontend/views/Pedigree/functions/useFetchPedigree.ts
+++ src/frontend/views/Pedigree/functions/useFetchPedigree.ts
@@ -7,19 +7,28 @@ export const useFetchPedigree = async ({
   setWhales,
   setNodes,
   setEdges,
+  loading,
+  setLoading,
 }: {
   id: string;
   setPedigree: Function;
   setWhales: Function;
   setNodes: Function;
   setEdges: Function;
+  loading: boolean;
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
+  setLoading(true);
   const pedigree = await fetchPedigree({ id });
-  if (!pedigree) throw new Error("Pedigree not found");
+  if (!pedigree) {
+    setLoading(false);
+    return;
+  }
   const whaleIds = pedigree.nodes.map((node) => node.id);
   const whales = await fetchWhales({ ids: whaleIds });
   setPedigree({ name: pedigree.name, id: pedigree.id });
   setWhales(whales);
   setNodes(pedigree.nodes);
   setEdges(pedigree.edges);
+  setLoading(false);
 };
