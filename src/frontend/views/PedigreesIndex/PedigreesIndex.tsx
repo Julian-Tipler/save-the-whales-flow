05086aa0f580215263eefd482a9af830f5d217d4
Julian <tipler.julian@gmail.com>
2023-08-06 12:04:38 -0500

    changed local rules and added an admin check for the icon

--- src/frontend/views/PedigreesIndex/PedigreesIndex.tsx
+++ src/frontend/views/PedigreesIndex/PedigreesIndex.tsx
@@ -5,10 +5,13 @@ import { Flex } from "@chakra-ui/react";
 import { usePedigreesIndexContext } from "./context/PedigreesIndexContext";
 import { Pedigree } from "../../../db/Types/Entities";
 import { PedigreeItem } from "./PedigreeItem";
+import { useAuthContext } from "../../auth/context/AuthContext";
 
 export const PedigreesIndex = () => {
   const { pedigrees, createPedigreeResolver, fetchPedegreesResolver } =
     usePedigreesIndexContext();
+
+  const { admin } = useAuthContext();
 
   useEffect(() => {
     fetchPedegreesResolver();
@@ -25,15 +28,17 @@ export const PedigreesIndex = () => {
           );
         })}
       </Flex>
-      <AddIcon
-        marginLeft={"4px"}
-        color={"green.500"}
-        onClick={() =>
-          createPedigreeResolver({
-            data: { name: "new pedigree", nodes: [], edges: [] },
-          })
-        }
-      />
+      {admin && (
+        <AddIcon
+          marginLeft={"4px"}
+          color={"green.500"}
+          onClick={() =>
+            createPedigreeResolver({
+              data: { name: "new pedigree", nodes: [], edges: [] },
+            })
+          }
+        />
+      )}
     </>
   );
 };
