878d921b52d5aed0235d97a80fa41de9e0254987
Julian <tipler.julian@gmail.com>
2023-08-06 12:38:13 -0500

    text overflow

--- src/frontend/views/Pedigree/WhalesIndex.tsx
+++ src/frontend/views/Pedigree/WhalesIndex.tsx
@@ -1,7 +1,4 @@
 import {
-  Box,
-  List,
-  ListItem,
   Table,
   TableCaption,
   Tbody,
@@ -13,6 +10,11 @@ import {
 } from "@chakra-ui/react";
 import { Card } from "../../components/Card";
 import { Whale } from "../../../db/Types/Entities";
+import { Link } from "react-router-dom";
+
+const tableCellStyle = {
+
+};
 
 export function WhalesIndex({ whales }: { whales: Whale[] }) {
   return (
@@ -20,21 +22,31 @@ export function WhalesIndex({ whales }: { whales: Whale[] }) {
       <Table variant="simple">
         <Thead>
           <Tr>
-            <Th>Status</Th>
-            <Th>Name</Th>
-            <Th>Gender</Th>
-            <Th>Born</Th>
-            <Th>Died</Th>
+            <Th style={tableCellStyle}>Id</Th>
+            <Th style={tableCellStyle}>Name</Th>
+            <Th style={tableCellStyle}>Status</Th>
+            <Th style={tableCellStyle}>Gender</Th>
+            <Th style={tableCellStyle}>Born</Th>
+            <Th style={tableCellStyle}>Died</Th>
           </Tr>
         </Thead>
         <Tbody>
           {whales.map((whale: Whale, index: number) => (
             <Tr key={index}>
-              <Td>{!whale.died ? "Alive" : "Deceased"}</Td>
-              <Td>{whale.name}</Td>
-              <Td>{whale.gender}</Td>
-              <Td>{whale.born}</Td>
-              <Td>{whale.died}</Td>
+              <Td>
+                <Link to={`/whales/${whale.id}`}>
+                  <Text style={{ display: "inline" }}>
+                    {whale.identification ? whale.identification : "<no id>"}
+                  </Text>
+                </Link>
+              </Td>
+              <Td style={tableCellStyle}>{whale.name}</Td>
+              <Td style={tableCellStyle}>
+                {!whale.died ? "Alive" : "Deceased"}
+              </Td>
+              <Td style={tableCellStyle}>{whale.gender}</Td>
+              <Td style={tableCellStyle}>{whale.born}</Td>
+              <Td style={tableCellStyle}>{whale.died}</Td>
             </Tr>
           ))}
         </Tbody>
